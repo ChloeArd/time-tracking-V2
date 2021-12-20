725c4a02dd974d7f8740c18d4cea7e60ddda0a43
ChloeArd <chloe.ardoise@gmail.com>
2021-12-20 10:24:02 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="./build/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/351e9300a0.js" crossorigin="anonymous"></script>
+    <script src="./build/js/front.js" defer></script>
+</head>
+<body>
+
+    <main>
+        <h1 class="center">Time Tracking <i class="far fa-clock"></i></h1>
+        <div class="border flexRow align">
+            <p class="question">Voulez-vous ajoutez un projet ?</p>
+            <a href="#" class="addProject align"><i class="fas fa-plus-square"></i> Projet</a>
+        </div>
+        <div id="projects" class="flexCenter flexRow wrap">
+            <div class="project flexColumn">
+                <h2 class="center">Titre</h2>
+                <div class="flexRow width_100 pad15">
+                    <div class="flexColumn width_20 flexCenter">
+                        <div class="time center">
+                            <i class="far fa-clock"></i>
+                            <p>Heure</p>
+                        </div>
+                        <div class="time center">
+                            <i class="far fa-calendar-alt"></i>
+                            <p>Date</p>
+                        </div>
+                    </div>
+                    <div class="flexColumn width_80 containerList scroller">
+                        <div class="width_100 flexRow list">
+                            <p class="width_90">Tâches à éffectuer</p>
+                            <i class="fas fa-stopwatch width_10 center"></i>
+                        </div>
+                        <div class="lineHorizontal"></div>
+                    </div>
+                </div>
+                <div class="flexRow align pad15">
+                    <i class="far fa-trash-alt edit"></i>
+                    <i class="far fa-eye edit"></i>
+                    <i class="fas fa-plus-square edit"></i>
+                </div>
+            </div>
+
+            <div class="project flexColumn">
+                <h2 class="center">Titre</h2>
+                <div class="flexRow width_100 pad15">
+                    <div class="flexColumn width_20 flexCenter">
+                        <div class="time center">
+                            <i class="far fa-clock"></i>
+                            <p>Heure</p>
+                        </div>
+                        <div class="time center">
+                            <i class="far fa-calendar-alt"></i>
+                            <p>Date</p>
+                        </div>
+                    </div>
+                    <div class="flexColumn width_80 containerList scroller">
+                        <div class="width_100 flexRow list">
+                            <p class="width_90">Tâches à éffectuer</p>
+                            <i class="fas fa-stopwatch width_10 center"></i>
+                        </div>
+                        <div class="lineHorizontal"></div>
+                    </div>
+                </div>
+                <div class="flexRow align pad15">
+                    <i class="far fa-trash-alt edit"></i>
+                    <i class="far fa-eye edit"></i>
+                    <i class="fas fa-plus-square edit"></i>
+                </div>
+            </div><div class="project flexColumn">
+                <h2 class="center">Titre</h2>
+                <div class="flexRow width_100 pad15">
+                    <div class="flexColumn width_20 flexCenter">
+                        <div class="time center">
+                            <i class="far fa-clock"></i>
+                            <p>Heure</p>
+                        </div>
+                        <div class="time center">
+                            <i class="far fa-calendar-alt"></i>
+                            <p>Date</p>
+                        </div>
+                    </div>
+                    <div class="flexColumn width_80 containerList scroller">
+                        <div class="width_100 flexRow list">
+                            <p class="width_90">Tâches à éffectuer</p>
+                            <i class="fas fa-stopwatch width_10 center"></i>
+                        </div>
+                        <div class="lineHorizontal"></div>
+                    </div>
+                </div>
+                <div class="flexRow align pad15">
+                    <i class="far fa-trash-alt edit"></i>
+                    <i class="far fa-eye edit"></i>
+                    <i class="fas fa-plus-square edit"></i>
+                </div>
+            </div>
+            <div class="project flexColumn">
+                <h2 class="center">Titre</h2>
+                <div class="flexRow width_100 pad15">
+                    <div class="flexColumn width_20 flexCenter">
+                        <div class="time center">
+                            <i class="far fa-clock"></i>
+                            <p>Heure</p>
+                        </div>
+                        <div class="time center">
+                            <i class="far fa-calendar-alt"></i>
+                            <p>Date</p>
+                        </div>
+                    </div>
+                    <div class="flexColumn width_80 containerList scroller">
+                        <div class="width_100 flexRow list">
+                            <p class="width_90">Tâches à éffectuer</p>
+                            <i class="fas fa-stopwatch width_10 center"></i>
+                        </div>
+                        <div class="lineHorizontal"></div>
+                    </div>
+                </div>
+                <div class="flexRow align pad15">
+                    <i class="far fa-trash-alt edit"></i>
+                    <i class="far fa-eye edit"></i>
+                    <i class="fas fa-plus-square edit"></i>
+                </div>
+            </div>
+
+        </div>
+    </main>
+
+</body>
+</html>
