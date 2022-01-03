f51a9c95f894f918f83399cf0abd78451fdb72bb
ChloeArd <chloe.ardoise@gmail.com>
2022-01-03 10:13:31 +0100

    time tracking

--- public/View/deleteProject.php
+++ public/View/deleteProject.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Accueil</title>
+    <title>Supprimer un projet</title>
     <link rel="stylesheet" href="../build/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/351e9300a0.js" crossorigin="anonymous"></script>
